c01375d8d58fb1c28fce1791d417284cf8d4b49c
Ronny Mandal <ronnyma@transfinite.no>
2017-04-10 17:39:10 +0200

    Done assignment 3!

--- ex3/predict.m
+++ ex3/predict.m
@@ -21,12 +21,12 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m, 1) X];
 
+a2 = sigmoid(X * Theta1');
+a3 = sigmoid([ones(m, 1) a2] * Theta2');
 
-
-
-
-
+[~, p] = max(a3, [], 2);
 
 
 % =========================================================================
