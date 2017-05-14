8c387e02c877130d322bc0f94185e3ca472674fb
Ronny Mandal <ronnyma@transfinite.no>
2017-05-14 21:37:17 +0200

    Part 2/6 done!
    Select Threshold |  15 /  15 | Nice work!

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -1,4 +1,4 @@
-function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+function [bestEpsilon, bestF1] = selectThreshold(yval, pval)
 %SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
 %outliers
 %   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
@@ -22,9 +22,18 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
+    
+    predictions = (pval < epsilon);
+    
+    tp = sum(predictions == yval & (yval == 1));
+    fp = sum((predictions == 1) & (yval == 0));
+    fn = sum((predictions == 0) & (yval == 1));
 
-
-
+        
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+    
+    F1 = (2 * prec * rec) / (prec + rec);
 
 
 
