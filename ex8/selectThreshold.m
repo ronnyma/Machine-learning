0bd80500c6dba11db699b83f34cb7304b8c23357
Ronny Mandal <ronnyma@transfinite.no>
2017-05-14 21:41:24 +0200

    Tidy code.

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -35,15 +35,6 @@ for epsilon = min(pval):stepsize:max(pval)
     
     F1 = (2 * prec * rec) / (prec + rec);
 
-
-
-
-
-
-
-
-
-
     % =============================================================
 
     if F1 > bestF1
